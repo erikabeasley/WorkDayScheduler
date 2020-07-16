996f90565c191013b8344c3ed3cbefc0258cf0e7
Erika <erika@beasleys.com>
2020-07-15 22:02:26 -0400

    Write Javascript and fix html

---
+++ script.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
+    //display current day & time.
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+    //Assign saveBtn click listener for user input and time stp??
+    $(".saveBtn").on("click", function () {
+        //get nearby values.
+        console.log(this);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        //set items in local storage.
+        localStorage.setItem(time, text);
+    })
+    //load any saved data from LocalStorage - do this for each hour created.
+    $("#time8 .description").val(localStorage.getItem("time8"));
+    $("#time9 .description").val(localStorage.getItem("time9"));
+    $("#time10 .description").val(localStorage.getItem("time10"));
+    $("#time11 .description").val(localStorage.getItem("time11"));
+    $("#time12 .description").val(localStorage.getItem("time12"));
+    $("#time13 .description").val(localStorage.getItem("time13"));
+    $("#time14 .description").val(localStorage.getItem("time14"));
+    $("#time15 .description").val(localStorage.getItem("time15"));
+    $("#time16 .description").val(localStorage.getItem("time16"));
+    $("#time17 .description").val(localStorage.getItem("time17"));
+    $("#time18 .description").val(localStorage.getItem("time18"));
+
+
+    function hourTracker() {
+        //get current number of hours.
+        var currentHour = moment().hour();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
+            console.log(hourBlock, currentHour)
+
+            //check if we've moved past this time
+            if (hourBlock < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (hourBlock === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    hourTracker();
+})
